6573331d5cde732b33b2c86e5bf83387f2e8ca3f
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-09-20 20:34:42 -0500

    added social icons linkedin and facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  
+  def facebook_like
+    content_tag :iframe, nil, 
+      :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=450&action=like&font=arial&colorscheme=dark&height=80",
+      :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => '100%'
+  end
 end
