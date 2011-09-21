877470ba3896d33a79ab82f4477f457b3e5bb5f8
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-09-20 23:43:00 -0500

    added helper for link_to

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
       :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=450&action=like&font=arial&colorscheme=dark&height=80",
       :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => '100%'
   end
+  
+ 
+  def build_link
+    link_to "eddielopezzzz.heroku.com", "http://eddielopezzz.heroku.com" 
+  end
+  
 end
