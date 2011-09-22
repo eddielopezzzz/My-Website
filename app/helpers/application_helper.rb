c02b3f6cdf03ed07d71b3f87322c932f14e6026c
Eddie Lopez <eddielopezzzz@hotmail.com>
2011-09-21 22:58:29 -0500

    added new background, sharethis social icons, added new social icon partial layout, rename heroku app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,14 @@ module ApplicationHelper
   
   def facebook_like
     content_tag :iframe, nil, 
-      :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=button_count&show_faces=true&width=450&action=like&font=arial&colorscheme=dark&height=80",
+      :src => "http://www.facebook.com/plugins/like.php?app_id=141440452603597&href=#{CGI::escape(request.url)}&send=false&layout=box_count&show_faces=false&action=like&font=arial&colorscheme=light&locale=en_US",
       :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like, :width => '100%'
   end
   
+
  
   def build_link
-    link_to "eddielopezzzz.heroku.com", "http://eddielopezzz.heroku.com" 
+    link_to "eddie.heroku.com", "http://eddie.heroku.com" 
   end
   
 end
