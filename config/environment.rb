8126ffbc1774850e6dac35bded58e1c3381e52db
Eduardo Lopez <eddielopezzzz@hotmail.com>
2011-12-17 00:11:06 -0600

    Added some stuff

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Website::Application.initialize!
+
+ENV['RECAPTCHA_PUBLIC_KEY']='6LeRR8sSAAAAAOzjYtH_n1FrW2Qhff8TPMIpo0VL'
+ENV['RECAPTCHA_PRIVATE_KEY']='6LeRR8sSAAAAABvYoEfvixiayNlsQK7f2orQx0yI'
