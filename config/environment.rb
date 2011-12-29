08a427c21aebeb6af2f6a18fa6fc2dffa241c5e9
Eduardo Lopez <eddielopezzzz@hotmail.com>
2011-12-29 17:52:15 -0600

    Added this project to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,5 @@ Website::Application.initialize!
 
 ENV['RECAPTCHA_PUBLIC_KEY']='6LeRR8sSAAAAAOzjYtH_n1FrW2Qhff8TPMIpo0VL'
 ENV['RECAPTCHA_PRIVATE_KEY']='6LeRR8sSAAAAABvYoEfvixiayNlsQK7f2orQx0yI'
+
+ActionMailer::Base.delivery_method = :smtp 
