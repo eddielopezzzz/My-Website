d4f63bd0ce3d8f806bc36ee1e044badce2f457a4
Eduardo Lopez <eddielopezzzz@hotmail.com>
2011-12-29 20:50:54 -0600

    Added smtp settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,15 @@ Website::Application.configure do
 
   config.active_support.deprecation = :notify
   
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.live.com",
+    :port => 587,
+    :user_name => "eddielopezzzz@hotmail.com",
+    :password => "Kristian1",
+    :tls => true
+  }
   
 end
